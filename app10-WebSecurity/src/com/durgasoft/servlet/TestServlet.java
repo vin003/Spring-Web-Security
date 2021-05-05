4520dbe6a8c0060b1cbbe4b3e73a8a9ef1d24465
vineet <vineetanything@gmail.com>
2021-05-05 19:54:27 +0530

    Initial commit

---
+++ app10-WebSecurity/src/com/durgasoft/servlet/TestServlet.java
@@ -0,0 +1,33 @@
+package com.durgasoft.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+   		response.setContentType("text/html"); 
+   		
+   		PrintWriter out = response.getWriter() ;
+   		out.print("<html><body>");
+   		out.print("<h1>Test Servlet</h1>");
+   		out.print("</body</html>");
+   		
+	}
+
+	
+
+}
